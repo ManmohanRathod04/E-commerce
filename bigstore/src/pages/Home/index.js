8817907a5d4cae07e37cb19a16bdae98bb43c0d8
ManmohanRathod04 <manmohanrathod421@gmail.com>
2021-09-15 16:39:19 +0530

    first commit

---
+++ bigstore/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+// import SpecialOffers from "../components/SpecialOffers";
+import SpecialOffers from "../../components/SpecialOffers";
+
+
+const Home = () => {
+    return (
+        <SpecialOffers/>
+    );
+}
+
+export default Home;
